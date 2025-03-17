36daa66a3d519a64e068e7f08821338cc9a1d455
Timothy Yeh <timothyeh666@gmail.com>
2025-03-18 00:49:04 +0800

    Formualte AI generation plan

--- image-generate.py
+++ image-generate.py
@@ -1,1 +1,3 @@
-print("This is to generate iamges of clothes")
+# Text-to-Image -> Generates an initial cartoon-style clothing piece based on the user's description.
+# Neural Style Transer -> Refines the uploaded clothing iamge to match a specific cartoon style
+# Combination ->Merge both outputs to get the final cartoonish clothing piece.
